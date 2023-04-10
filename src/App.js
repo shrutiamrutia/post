41e6ca74f5c6ca984c8d7491a5beec343b27b964
shrutiamrutia <shrutiamrutiya.albetrios@gmail.com>
2023-04-10 10:05:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter } from "react-router-dom"
+import Home from "./Home"
+import About from "./About"
+import Contact from "./Contact"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
